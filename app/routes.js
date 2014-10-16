d490e6d302d0f89cf626dbbc11efbfbaed8091d8
Josh Lankford <lankford.josh@gmail.com>
2014-10-16 15:50:24 -0700

    add server and basic routes

---
+++ app/routes.js
@@ -0,0 +1,28 @@
+module.exports = function(app) {
+
+	// server routes ===========================================================
+	// handle things like api calls
+	// authentication routes
+
+	// frontend routes =========================================================
+	// route to handle all angular requests
+	app.get('/', function(req, res) {
+		res.sendfile('./public/index.html');
+	});
+
+	app.get('/user/1', function(req, res){
+		res.send({
+			fName: 'Josh',
+			lName: 'Lankford',
+			language: 'JavaScript'
+		})
+	})
+
+	app.get('/company/1', function(req, res){
+		res.send({
+			cName: 'Example',
+			language: 'JavaScript'
+		})
+	})
+
+};
