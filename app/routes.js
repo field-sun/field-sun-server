a4657b278cbb302a61b04e610047259cee8b2f67
Josh Lankford <lankford.josh@gmail.com>
2014-10-20 11:53:23 -0700

    Add all DS_Store to gitignore

--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,10 @@
 module.exports = function(app) {
 
-	// server routes ===========================================================
-	// handle things like api calls
-	// authentication routes
-
-	// frontend routes =========================================================
-	// route to handle all angular requests
 	app.get('/', function(req, res) {
 		res.sendfile('./public/index.html');
 	});
 
-	app.get('/user/1', function(req, res){
+	app.get('/api/user/1', function(req, res){
 		res.send({
 			fName: 'Josh',
 			lName: 'Lankford',
@@ -19,12 +13,16 @@ module.exports = function(app) {
 		})
 	})
 
-	app.get('/company/1', function(req, res){
+	app.get('/api/company/1', function(req, res){
 		res.send({
 			cName: 'Example',
 			language: 'JavaScript',
 			location: 'San Francisco'
 		})
+	})
+
+	app.get('*', function(req, res){
+		res.sendfile('./public/views/404.html')
 	})
 
 };
