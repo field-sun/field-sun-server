f73d7cdf85a63c115a4bad19bc65e914fa8a4166
Greg Ferrell <gregerg@gmail.com>
2014-10-26 14:46:27 -0700

    now allowing cross domain requests

--- server.js
+++ server.js
@@ -5,16 +5,23 @@ var methodOverride = require('method-override');
 var knex 		   = require('./config/db');
 var port 		   = process.env.PORT || 8080;
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(allowCrossDomain);
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(__dirname + '/public'));
 require('./routes/routes')(app);
 
 var bookshelf = require('./config/db');
 
-app.listen(port);	
+app.listen(port);
 console.log('ENGNR API running on: ' + port);
 exports = module.exports = app;
