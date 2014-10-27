688c80a9bd1412f93efa1865a6f5330b4ec2420a
Josh Lankford <lankford.josh@gmail.com>
2014-10-27 15:36:37 -0700

    add origin and accept

--- routes/routes.js
+++ routes/routes.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Orign, Accept, Content-Type, Authorization, Content-Length, X-Requested-With');
   next();
  });
 
