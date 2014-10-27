d3154929092c92e276f676bf1739ed7d502aa2a2
Josh Lankford <lankford.josh@gmail.com>
2014-10-27 15:29:14 -0700

    add post access

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
 
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Methods', 'GET,POST');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   next();
  });
