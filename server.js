a8007cf12d0c63af36b0b7c378269db04bd0276b
Josh Lankford <lankford.josh@gmail.com>
2014-10-27 12:50:03 -0700

    Allow authorization

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var port 		   = process.env.PORT || 8080;
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     next();
 }
 
