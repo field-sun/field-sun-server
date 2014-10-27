554840f81e6a6f91f42cc67aa086e6cc58a4cb73
Josh Lankford <lankford.josh@gmail.com>
2014-10-27 15:25:57 -0700

    add auth

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,7 @@ module.exports = function(app) {
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   next();
  });
 
