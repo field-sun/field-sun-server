229b86c3bdef9c2d6aceda22c40a2c886fd115af
Josh Lankford <lankford.josh@gmail.com>
2014-10-27 15:23:11 -0700

    try app.all with *

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ var knex = require('../config/knex')
 
 module.exports = function(app) {
 
-app.all('/', function(req, res, next) {
+app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
