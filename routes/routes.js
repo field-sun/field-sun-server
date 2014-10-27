7e53ffe7ae9b22851efa71ab7833d27f5335f24c
Josh Lankford <lankford.josh@gmail.com>
2014-10-27 15:53:56 -0700

    add localhost origin

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,7 @@ module.exports = function(app) {
 
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Orign, Accept, Content-Type, Authorization, Content-Length, X-Requested-With');
   next();
